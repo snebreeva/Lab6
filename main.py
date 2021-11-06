8137be42a809ad051922aec1ff7a217ee0c229a9
snebreeva <91993969+snebreeva@users.noreply.github.com>
2021-11-06 20:45:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-def medians(nums1, nums2):
-    a = []
-    a = nums1 + nums2
-    a1 = sorted(a)
-    b = len(a1)
-    # если это нулевые списки, значит где-то закралась ошибка
-    if b == 0:
-        print('бегом переделывать')
-    # теперь проверим длину списка
-    elif b % 2 == 1:
-        m = a1[b // 2]
-    elif b % 2 == 0:
-        m = (a1[b // 2 ] + a1[b // 2 - 1]) / 2
-    # выведем результат
-    print(m)
-
+def rot(num):
+    num = str(num)
+    numeric = list(num)
+    for i in range(len(numeric)):
+        if numeric[i] == '6':
+            numeric[i] = '9'
+            print("".join(numeric))
+            break
+        elif i == len(numeric)-1:
+            print("".join(numeric))
 
 if __name__ == "__main__":
-    nums1, nums2 =  [5, 5], [4, 4]
-    medians(nums1, nums2)
+    num = 999
+    rot(num)
